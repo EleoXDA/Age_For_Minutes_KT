bf5f9b3a851288ebbe3e5fb6fdb745ffdb60a0c2
Elbay Malik <eleo.666@gmail.com>
2022-03-30 17:20:58 +0200

    Add button function

--- app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
+++ app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
@@ -2,10 +2,20 @@ package eu.eleo.ageforminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class  MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener { calculateDate() }
+    }
+    private fun calculateDate() {
+        //select date of birth from user
+        //calculate the days between now and them
+        //convert days to hours by *24*60
+        //display the time in box
     }
 }
