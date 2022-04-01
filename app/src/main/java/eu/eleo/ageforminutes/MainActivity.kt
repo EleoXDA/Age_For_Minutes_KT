13596505d68d5658e8e139dd2a472867cc975ed1
Elbay Malik <eleo.666@gmail.com>
2022-04-01 22:15:37 +0200

    Add toast to click listener

--- app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
+++ app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
@@ -1,21 +1,22 @@
 package eu.eleo.ageforminutes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener { calculateDate() }
+        val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
+        buttonDatePicker.setOnClickListener {
+            Toast.makeText(this,
+                "Date Picker pressed", Toast.LENGTH_SHORT).show()
+        }
     }
+
     private fun calculateDate() {
-        //select date of birth from user
-        //calculate the days between now and them
-        //convert days to hours by *24*60
-        //display the time in box
     }
 }
