b04d3790073e5920953878340830e2ee9172c61e
Elbay Malik <eleo.666@gmail.com>
2022-04-01 22:57:34 +0200

    Add date picker and test with toast

--- app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
+++ app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
@@ -3,16 +3,22 @@ package eu.eleo.ageforminutes
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var selectedDate : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
+        selectedDate = findViewById(R.id.selectedDate)
+
         buttonDatePicker.setOnClickListener { datePicker() }
     }
 
@@ -22,10 +28,18 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
         DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
-                view, year, month, dayOfMonth ->
+                view, selectedYear, selectedMonth, selectedDayOfMonth ->
             Toast.makeText(this,
-                "Date Picker works", Toast.LENGTH_SHORT).show()
-                                                                  }
-            , 2020, 1, 1).show()
+                "Year $selectedYear, ${selectedMonth+1}.month, and $selectedDayOfMonth day was picked",
+                Toast.LENGTH_SHORT).show()
+
+            val selectDate ="$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+            selectedDate?.text = selectDate
+                                                                  },
+            year,
+            month,
+            day
+        ).show()
     }
 }
