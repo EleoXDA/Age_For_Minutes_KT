516cb7b0c2d17e7fc7a7a44a8de9da11ef4c4aea
Elbay Malik <eleo.666@gmail.com>
2022-04-01 22:30:44 +0200

    Add date picker and test with toast

--- app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
+++ app/src/main/java/eu/eleo/ageforminutes/MainActivity.kt
@@ -1,9 +1,11 @@
 package eu.eleo.ageforminutes
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
-        buttonDatePicker.setOnClickListener {
-            Toast.makeText(this,
-                "Date Picker pressed", Toast.LENGTH_SHORT).show()
-        }
+        buttonDatePicker.setOnClickListener { datePicker() }
     }
 
-    private fun calculateDate() {
+    private fun datePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            Toast.makeText(this,
+                "Date Picker works", Toast.LENGTH_SHORT).show()
+                                                                  }
+            , 2020, 1, 1).show()
     }
 }
